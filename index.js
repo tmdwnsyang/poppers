5e1116069770b4447402587d811e4b85df60403c
tmdwns <tmdwns.yang@gmail.com>
2022-07-19 16:16:19 -0700

    Update index.js
    Rewrote to improve encapsulation... looking at you, global vars

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import game from "./game.js";
 
-
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js';
-import { getDatabase, ref, set, push, onValue, query, orderByChild} from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-database.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+import { getDatabase, ref, set,push,onValue,query,  orderByChild,
+} from "https://www.gstatic.com/firebasejs/9.9.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDL5zVdi28woZKwXaqQGn488oaV4pfi5Fk",
@@ -20,213 +20,226 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-
 // var leaderBoardsRaw = fs.readFile('score.json');
 // var leaderBoards = JSON.parse(leaderBoardsRaw);
 // fetch('https://tmdwnsyang.github.io/poppers/score.json')
 // .then(response => {
-//   if (!response.ok) 
+//   if (!response.ok)
 //     throw new Error(`HTTP error: ${response.status}`);
 //   else{
 //     return response.json();
 //   }}).then( data => {
 //     leaderBoards = data;
-    
+
 //     // console.log(leaderBoards);
 //   } ).catch( error => {
 //     console.error(`could not get scores json: ${error}`);
 //   })
+initializeGame();
 
+function initializeGame() {
+  const levels = document.getElementsByClassName("levelTile");
+  const container = document.querySelector("#container");
+  let debug = true;
+  let gameObj = new game(easy, debug);
+  // MENU Options!! 0~6
+  menuSelectClickHandler(levels[0], gameObj);
+  menuSelectClickHandler(levels[1], gameObj);
+  menuSelectClickHandler(levels[2], gameObj);
 
-//! linebreaks and other formatting stuff
-var scoreCounter = document.querySelector("#subT");
-const idLevels = document.querySelector("#levels");
-const levels = document.getElementsByClassName("levelTile");
-const container = document.querySelector("#container");
-var tiles = document.getElementsByClassName("tile");
-let debug = true;
-let gameObj = new game(easy, debug);
-let leaderBoard
+  levels[3].addEventListener("click", () => openPopup("scoreBoard"));
+  levels[4].addEventListener("click", () => openPopup("share"));
+  levels[5].addEventListener("click", openPopup);
+  gameInProgressHandler(container, gameObj);
+}
 
-// MENU Options!! 0~6 
-levels[0].addEventListener("click",  difficultySelection);
-levels[1].addEventListener("click",  difficultySelection);
-levels[2].addEventListener("click", difficultySelection);
-levels[3].addEventListener("click", ()=> openPopup('scoreBoard'));
-levels[4].addEventListener("click", ()=> openPopup('share'));
-levels[5].addEventListener("click", openPopup);
-container.addEventListener("click", backgroundChange);
-container.addEventListener("click", popperGameInProgress);
+function backgroundChange(event, gameObj) {
+  event.target.style.backgroundColor = gameObj.bgChange();
+}
 
-
-
-// Sets up the board with the number of rquired tiles.
+// Sets up the board with the number of required tiles.
 // difficultySelection Buttons will disappear once the difficulty has been made
-function difficultySelection(event) {
-  var difficulty = event.currentTarget.id;
-  console.log(difficulty);
-  closePopup();
-  gameObj.setGameProperties(difficulty);
-  for (let i = 0; i < gameObj.getBoardSize(); i++) {
-    var divTile = document.createElement("button");
-    divTile.className = "tile";
-    container.appendChild(divTile);
-  }
+function menuSelectClickHandler(elem, gameObj) 
+{
+  elem.addEventListener("click", function (event)
+   {
+    var tiles = document.getElementsByClassName("tile");
+    const idLevels = document.querySelector("#levels");
+    var scoreCounter = document.querySelector("#subT");
 
-  clearChild(idLevels);
-  for (let item of tiles) {
-    item.style.visibility = "visible";
-  }
-  scoreCounter.textContent = "Click anywhere below to begin!";
+    var difficulty = event.currentTarget.id;
+    closePopup();
+    gameObj.setGameProperties(difficulty);
+    for (let i = 0; i < gameObj.getBoardSize(); i++) {
+      var divTile = document.createElement("button");
+      divTile.className = "tile";
+      container.appendChild(divTile);
+    }
+
+    clearChild(idLevels);
+    for (let item of tiles) {
+      item.style.visibility = "visible";
+    }
+    scoreCounter.textContent = "Click anywhere below to begin!";
+  });
 }
 
 // theDiv represent the tiles that are being clicked. Upon clicks, the
 // texts will be updated. Also keeps score of the game.
-function popperGameInProgress() {
-  const theDiv = event.target;
 
-  var win = true;
-  scoreCounter.appendChild(document.createTextNode(`score: ${gameObj.getBoardScore()}`));
+function gameInProgressHandler(elem, gameObj) {
+  elem.addEventListener(
+    "click",
+    function handler(event) {
+      backgroundChange(event, gameObj);
 
-  gameObj.incrementBoardScore();
-
-  theDiv.textContent = "Pop!";
-  scoreCounter.textContent = `score: ${gameObj.getBoardScore()}`;
-  for (let i = 0; i < gameObj.getBoardSize() - 1 && gameObj.getBoardScore() > 0; i++) {
-    if (tiles[i].style.backgroundColor != tiles[i + 1].style.backgroundColor) {
-      console.log("Different color!");
-      win = false;
-      break;
-    } else win = true;
-  }
-  if (win) {
-    for (let item of tiles) {
-      item.textContent = "🥳";
-    }
-    // document.body.style.background = 'black';
-    this.removeEventListener("click", popperGameInProgress);
-    this.removeEventListener("click", backgroundChange);
-
-    resultsPage();
-  }
-}
-function backgroundChange() {
-  event.target.style.backgroundColor = gameObj.bgChange();
+      var tiles = document.getElementsByClassName("tile");
+      var scoreCounter = document.querySelector("#subT");
+      const theDiv = event.target;
+      var win = true;
+      scoreCounter.appendChild(
+        document.createTextNode(`score: ${gameObj.getBoardScore()}`)
+      );
+      gameObj.incrementBoardScore();
+      theDiv.textContent = "Pop!";
+      scoreCounter.textContent = `score: ${gameObj.getBoardScore()}`;
+      for (
+        let i = 0;
+        i < gameObj.getBoardSize() - 1 && gameObj.getBoardScore() > 0;
+        i++
+      ) {
+        if (
+          tiles[i].style.backgroundColor != tiles[i + 1].style.backgroundColor
+        ) {
+          console.log("Different color!");
+          win = false;
+          break;
+        } else win = true;
+      }
+      if (win) {
+        for (let item of tiles) {
+          item.textContent = "🥳";
+        }
+        // document.body.style.background = 'black';
+        this.removeEventListener("click", handler);
+        // this.removeEventListener("click", bgHandler);
+        resultsPage(gameObj);
+      }
+    },
+    false
+  );
 }
 
 // runs when the game is over.
-function resultsPage() {
-
-  //# ===== RESULT UI 
+function resultsPage(gameObj) {
+  //# ===== RESULT UI
   var tests = document.getElementsByClassName("hiddenTile");
-  var hiddenTile = document.querySelector('#tile2');
+  var hiddenTile = document.querySelector("#tile2");
   tests[0].style.visibility = "visible";
-  tests[0].style.display ="block";
+  tests[0].style.display = "block";
   tests[0].textContent = "YOU WON!";
   tests[0].style.color = "white";
   tests[0].style.backgroundColor = "#ff85dB";
 
   // Save all scores into a item file here
-  appendItemChild(tests[0],`Your score is ${gameObj.getBoardScore()}. ${gameObj.getEncouragement()}`,'div', 'subtextSmall');
+  appendItemChild(
+    tests[0],
+    `Your score is ${gameObj.getBoardScore()}. ${gameObj.getEncouragement()}`,
+    "div",
+    "subtextSmall"
+  );
 
   // probably not the most elegant way, but this adds 3 buttons in the result.
-  var str1=['Return','Share','Try Again'];
-  for( let i = 0; i < 3; i++)
-    appendItemChild(hiddenTile,str1[i], 'button','levelTile');
+  var str1 = ["Return", "Share", "Try Again"];
+  for (let i = 0; i < 3; i++)
+    appendItemChild(hiddenTile, str1[i], "button", "levelTile");
 
-  hiddenTile.children[1].addEventListener("click", () => {window.location.reload()});
-  hiddenTile.children[2].addEventListener("click", () => openPopup('share'));
-  
-  openPopup('gameOver');
+  hiddenTile.children[1].addEventListener("click", () => {
+    window.location.reload();
+  });
+  hiddenTile.children[2].addEventListener("click", () => openPopup("share"));
+
+  openPopup("gameOver");
 }
 
 //# ========== POPUP MENUS
-function openPopup(option = 'ERROR'){
+function openPopup(option = "ERROR") {
   let textHeading = document.createElement("h2");
   let windowPopup = document.getElementById("popup");
   windowPopup.classList.add("open-popup");
 
   clearChild(windowPopup);
-  if (option ==='scoreBoard')
-  {
+  if (option === "scoreBoard") {
     scoreboardPopup(windowPopup);
-  }
-  else if (option === 'gameOver')
-    gameOverPopup(windowPopup);
-  else if (option === 'share')
-    appendItemChild(windowPopup, 'Share, to whom?','h2');
-  else{
+  } else if (option === "gameOver") gameOverPopup(windowPopup);
+  else if (option === "share")
+    appendItemChild(windowPopup, "Share, to whom?", "h2");
+  else {
     textHeading.textContent = "Credits, credits, CREDITS!";
     windowPopup.appendChild(textHeading);
   }
-  if (option != 'gameOver'){
-    let dismissButton = appendItemChild(windowPopup, 'dismiss','button');
+  if (option != "gameOver") {
+    let dismissButton = appendItemChild(windowPopup, "dismiss", "button");
     dismissButton.addEventListener("click", closePopup);
   }
-  
 }
 
-function closePopup(){
-  let windowPopup = document.getElementById('popup');
-   windowPopup.classList.remove('open-popup');
+function closePopup() {
+  let windowPopup = document.getElementById("popup");
+  windowPopup.classList.remove("open-popup");
 }
-
-
-
 
 //# === POPUP: NAME INPUT DIALOGUE
-// Called by results() page. Responsible for saving the user game information 
+// Called by results() page. Responsible for saving the user game information
 // and saving to the cloud.
-function gameOverPopup (windowPopup){
+function gameOverPopup(windowPopup) {
   appendItemChild(windowPopup, "Let's make you famous!", "h2");
   appendItemChild(windowPopup, "Enter your name below.", "subText");
 
-  
   appendNewLineChild(windowPopup);
   var playerName = appendItemChild(windowPopup, null, "input");
   appendNewLineChild(windowPopup);
-  
 
   var submitButton = appendItemChild(windowPopup, "Submit!", "button");
 
-  submitButton.addEventListener('click',()=> {
-    gameObj.setPlayerProperties(playerName.value, gameObj.getBoardScore() );
+  submitButton.addEventListener("click", () => {
+    gameObj.setPlayerProperties(playerName.value, gameObj.getBoardScore());
     closePopup();
-    
-    writeUserData(gameObj.getPlayerName(), gameObj.getPlayerScoreCount(), 
-                  gameObj.getDifficulty(), 0);  
+
+    writeUserData(gameObj.getPlayerName(), gameObj.getPlayerScoreCount(),
+      gameObj.getDifficulty(), 0);
   });
-  
 }
 
-function scoreboardPopup(windowPopup){
+function scoreboardPopup(windowPopup) {
   appendItemChild(windowPopup, "Scoreboard", "h2");
-  var topThree = [ [], [], []];
-  const easyRef = query(ref(db, 'easy'), orderByChild('score'));
-  onValue(easyRef, (snapshot) => {
-    snapshot.forEach((childSnapshot) =>{
-      const childKey = childSnapshot.key;
-      const childData = childSnapshot.val();
-      console.log(`${childData.playerName} | ${childData.score}`);
-      // console.log(childKey);
-    });
-}, { onlyOnce: true});
-  
-
+  var topThree = [[], [], []];
+  const easyRef = query(ref(db, "easy"), orderByChild("score"));
+  onValue(
+    easyRef,
+    (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+        console.log(`${childData.playerName} | ${childData.score}`);
+        // console.log(childKey);
+      });
+    },
+    { onlyOnce: true }
+  );
 }
-
 
 //# FIREBASE DATABASE HELPER FUNCTIONS!
-function writeUserData( playerName, score, difficulty, time = 0)
-{
+// Takes player info and saves data to the cloud
+function writeUserData(playerName, score, difficulty, time = 0) {
   // const database = getDatabase(app);
   const postListRef = ref(db, difficulty);
   const newPostRef = push(postListRef);
   set(newPostRef, {
     difficulty: difficulty,
-    playerName : playerName,
-    score : score
-  })
+    playerName: playerName,
+    score: score,
+  });
   console.log(newPostRef);
 }
 
@@ -234,25 +247,27 @@ function writeUserData( playerName, score, difficulty, time = 0)
 // HELPER FUNCTIONS....
 // pre: takes in parent object with children
 // post: clears ALL children
-function clearChild(parent)
-{
-  while(parent.firstChild) {
+function clearChild(parent) {
+  while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
 }
 
 // post: takes in a parent element, item content to append, and type
 // pre: returns element type. Appends the item to the last child
-function appendItemChild(parent, textContent ='', elementType = 'div', className  =''){
+function appendItemChild(
+  parent,
+  textContent = "",
+  elementType = "div",
+  className = ""
+) {
   let item = document.createElement(elementType);
   item.textContent = textContent;
   parent.appendChild(item);
-  if (className.length != 0)
-    item.className = className; 
+  if (className.length != 0) item.className = className;
   return item;
 }
 
-function appendNewLineChild(parent)
-{
-  appendItemChild(parent, '', 'br');
+function appendNewLineChild(parent) {
+  appendItemChild(parent, "", "br");
 }
